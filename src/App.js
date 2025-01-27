897a019ab8a8a23f86cd8f0577a3c37db47e23e9
jeremy.zimmerman <jeremy.zimmerman@fdmgroup.com>
2025-01-26 19:48:12 -0700

    Adding project to Git repository

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SolitaireGame from "./components/SolitaireGame";
+
+const App = () => {
+  return (
+    <div className="app">
+      <SolitaireGame />
+    </div>
+  );
+};
+
+export default App;
